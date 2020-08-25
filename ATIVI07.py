0b502cdef65b47297632c8cec8b9bea2e6a29636
kauaas <57207681+kauaas@users.noreply.github.com>
2020-08-24 22:19:50 -0300

    Add files via upload

---
+++ ATIVI07.py
@@ -0,0 +1,9 @@
+Primeiro = int(input("informe o Primeiro do intervalo: "))
+Ultimo = int(input("informe o ultimo do intervalo: "))
+SOMA=0
+while (primeiro < ultimo - 1):
+    primeiro = primeiro+ 1
+    print(primeiro)
+    soma = (Primeiro+SOMA)
+
+print("a soma dos intervalos intervalos é = {}".format(SOMA))
